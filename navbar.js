3af0cb05c9fa328ed3589d0fdc575b52868cafe3
elouis080 <elouis080@gmail.com>
2025-12-09 04:29:34 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,19 +1,7 @@
 // hideNav.js
 let lastScrollTop = 0;
 const nav = document.querySelector('.nav');
-const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
-
-// Hamburger toggle
-hamburger.addEventListener('click', (e) => {
-  navLinks.classList.toggle('show');
-  e.stopPropagation();
-});
-document.addEventListener('click', (e) => {
-  if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) {
-    navLinks.classList.remove('show'); // close menu
-  }
-});
 
 // Hide navbar on scroll down
 window.addEventListener('scroll', () => {
